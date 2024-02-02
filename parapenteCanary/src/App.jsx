3269ebbf929de182c8be98b7942e3a2b77291d32
marionovaro <marionovaro04@gmail.com>
2024-02-02 10:36:28 +0100

    landing page

--- parapenteCanary/src/App.jsx
+++ parapenteCanary/src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css"
 const App = () => {
   return (
     <div id="app-container">
-      <h1>Hola mundo</h1>
+      <section id="landing-page">
+        <h1 id="landing-page-title">Parapente Canary</h1>
+      </section>
+      {/* <img id="landing-image" src="https://i.postimg.cc/g2hWsYqN/silueta-teide-sin-fondo.png" alt="background image parapente teide" /> */}
     </div>
   )
 }

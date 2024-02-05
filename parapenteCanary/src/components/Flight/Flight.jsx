2ec8989be35eb10f4b01c71977d93f80d9d653fb
marionovaro <marionovaro04@gmail.com>
2024-02-05 18:06:03 +0100

    flight info section

--- parapenteCanary/src/components/Flight/Flight.jsx
+++ parapenteCanary/src/components/Flight/Flight.jsx
@@ -3,7 +3,7 @@ import "./Flight.css"
 export const Flight = () => {
   return (
     <section id="flight-info-section">
-      {/* <img src="https://res.cloudinary.com/dx2arqne6/image/upload/v1707075457/kpao2jxkufj3zgxgepfg.jpg" alt="Teide From Space" /> */}
+      <img src="https://res.cloudinary.com/dx2arqne6/image/upload/v1707075457/kpao2jxkufj3zgxgepfg.jpg" alt="Teide From Space" />
     </section>
   )
 }

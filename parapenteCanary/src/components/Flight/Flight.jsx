05555e4041ac6d0dbcdef9c8e16f7b55894d721c
marionovaro <marionovaro04@gmail.com>
2024-02-05 18:05:53 +0100

    flight info section

--- parapenteCanary/src/components/Flight/Flight.jsx
+++ parapenteCanary/src/components/Flight/Flight.jsx
@@ -3,13 +3,7 @@ import "./Flight.css"
 export const Flight = () => {
   return (
     <section id="flight-info-section">
-      <div style={{position: "relative", width: "100%", height: "0", paddingTop: "56.2500%",
-      paddingBottom: "0", marginTop: "1.6em", marginBottom: "0.9em", overflow: "hidden",
-      borderRadius: "8px", willChange: "transform"}}>
-        <iframe loading="lazy" style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0", margin: "0"}}
-          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF7p9edTbE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-        </iframe>
-      </div>
+      {/* <img src="https://res.cloudinary.com/dx2arqne6/image/upload/v1707075457/kpao2jxkufj3zgxgepfg.jpg" alt="Teide From Space" /> */}
     </section>
   )
 }

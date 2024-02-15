626c444cac2fd8598fe38dd2619a6bd4ab85d32c
marionovaro <marionovaro04@gmail.com>
2024-02-15 21:35:52 +0100

    animation buy button

--- parapenteCanary/src/components/BuyButton/BuyButton.jsx
+++ parapenteCanary/src/components/BuyButton/BuyButton.jsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react"
 import "./BuyButton.css"
 
 export const BuyButton = () => {
+
+  useEffect(() => {
+    console.log(document.querySelector(".vuela-con-nosotros"))
+  
+    const observer = new IntersectionObserver(entries => {
+      console.log(entries)
+      //? las entries son el array de objetos que le hemos dicho que observe (observer.observe....), aunq solo haya 1, nos da un array
+      entries.forEach(entry => { //? recorremos el array para poder acceder a cada elemento (en este caso solo hay uno)
+        const buttonTarget = entry.target.querySelector(".vuela-con-nosotros")
+        //? entry es el objeto (dentro del array de objetos) que hemos dicho que observe, mientras que entry.target es el elemento como tal, al cual podemos acceder y modificar y tal
+        if (entry.isIntersecting) { //? isIntersecting es un boleano que nos dice si el objeto está en la viewport (es visible)
+          buttonTarget.classList.add("main-button-animation") //? si es true (se ve), le añadimos la 2a clase del elemento, a la que le hemos dado las animaciones
+        } else {
+          buttonTarget.classList.remove('main-button-animation')
+        }
+      })
+    })
+    let button = document.querySelector("#main-button-wrapper")
+    observer.observe(button)
+  
+  }, [])
+
   return (
+    <nav id="main-button-wrapper">
+      <button className="vuela-con-nosotros">
+        VUELA CON NOSOTROS!
+      </button>
+    </nav>
     // <button className="learn-more">
     //   <span className="circle" aria-hidden="true">
     //     <span className="icon arrow"></span>
@@ -14,11 +42,5 @@ export const BuyButton = () => {
     //   <span class="text">VUELA CONMIGO</span>
     //   <span aria-hidden="" class="marquee">VUELA CONMIGO</span>
     // </button>
-
-
-    <button className="vuela-con-nosotros">
-      VUELA CON NOSOTROS!
-    </button>
-
   )
 }

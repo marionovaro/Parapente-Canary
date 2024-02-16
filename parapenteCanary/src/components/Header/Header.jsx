f03daf04f5a7d8ce05ce8e65d75613509652b574
marionovaro <marionovaro04@gmail.com>
2024-02-16 18:37:51 +0100

    pasarela pago

--- parapenteCanary/src/components/Header/Header.jsx
+++ parapenteCanary/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   const navigate = useNavigate()
   return (
     <section id="header-container">
-      <div id="logo-container" onClick={() => navigate("/main")}>Logo</div>
+      <div id="logo-container" onClick={() => navigate("/")}>Logo</div>
       <nav id="header-nav">
         <div>Inicio</div>
         <div onClick={() => navigate("/compra")}>Reservas</div>

b06e6915756d1e3f537c43812012c3326b7c87b6
marionovaro <marionovaro04@gmail.com>
2024-02-19 17:01:05 +0100

    reestructurando metodologia

---
+++ parapenteCanary/src/components/CheckOutForm/CheckOutForm copy.jsx
@@ -0,0 +1,9 @@
+import axios from "axios"
+import { CardElement, useStripe, useElements, PaymentElement } from "@stripe/react-stripe-js"
+
+
+import "./CheckOutForm.css"
+
+export const CheckOutForm = () => {
+
+}
---
+++ parapenteCanary/src/pages/Compra/Compra copy.jsx
@@ -0,0 +1,13 @@
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+
+import { CheckOutForm } from "../../components"
+
+export const Compra = () => {
+
+  return (
+    <>
+    
+    </>
+  )
+}

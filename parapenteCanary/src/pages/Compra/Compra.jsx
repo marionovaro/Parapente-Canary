76396bf79b9fb24070c61dc8be24ccfee7aa7969
marionovaro <marionovaro04@gmail.com>
2024-02-21 19:06:28 +0100

    push

--- parapenteCanary/src/pages/Compra/Compra.jsx
+++ parapenteCanary/src/pages/Compra/Compra.jsx
@@ -1,30 +1,51 @@
-// import { loadStripe } from "@stripe/stripe-js"
-// import { Elements } from "@stripe/react-stripe-js"
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 // import { CheckOutForm } from "../../components"
-// import "./Compra.css"
+import "./Compra.css"
+import { CheckOutForm } from "../../components/CheckOutForm/CheckOutForm copy";
 
-// //? nos estamos conectando a Stripe, esta clave nos la dan cuando hacemos el registro en Stripe
-// const stripePromise = loadStripe("pk_test_51OkONdKjqtTHurvUtex6Vr0Mm4xmEdLpzqZjL5OKUfgdE79YbASXP7dQl9F65QOfDIeVlhScU3OEexYWkMvS2IuG00aoNBt41J")
+const stripePromise = loadStripe("pk_test_51OkONdKjqtTHurvUtex6Vr0Mm4xmEdLpzqZjL5OKUfgdE79YbASXP7dQl9F65QOfDIeVlhScU3OEexYWkMvS2IuG00aoNBt41J") //? hay que llamar a loadstripe fuera de la función para evitar que se recree el stripe en cada render
 
-// export const Compra = () => {
+export const Compra = () => {
+  const [clientSecret, setClientSecret] = useState("");
 
-//   (async () => {
-//     const response = await fetch("/secret");
-//     const { client_secret: clientSecret } = await response.json();
-//   })
+  useEffect(() => { //? creamos un intent nada más cargar nuestra pantalla de checkout, y cogemos el clientSecret del backend (lo necesitamos en el front)
+    const fetchPayment = fetch("http://localhost:3000/create-payment-intent", { //? vamos al endpoint que hemos seteado al hacer el intent en el back
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',
+     },
+      body: JSON.stringify({ items: [{ id: "vuelo-parapente"}]}),
+    })
+    .then((res) => res.json()) 
+    .then((data) => setClientSecret(data.clientSecret)); //? seteamos el clientSecret como estado
+  }, [])
 
-//   const options = {
-//     clientSecret: clientSecret
-//   }
-//   return (
-//     <div id="shop-page">
-//     {/* <h1>Hola</h1> */}
-//     <Elements stripe={stripePromise} options={options}>
-//       <div className="payment-card-wrapper">
-//         <CheckOutForm/>
-//       </div>
-//     </Elements>
-//     </div>
-//   )
-// }
+  //? ------- OPCIONES Y APARIENCIA ---------
+  const appearance = {
+    theme: "night",
+    // labels: "floating",
+    variables: {
+      // colorPrimary: "#FE981E",
+    }
+  }
+
+  const options = {
+    clientSecret,
+    appearance
+  }
+
+
+  return (
+    <div id="shop-page">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckOutForm/>
+        </Elements>
+      )}
+    </div>
+  )
+}
--- parapenteCanary/src/routes/routes.jsx
+++ parapenteCanary/src/routes/routes.jsx
@@ -1,9 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "../App";
-import { Compra } from "../pages/Compra/Compra copy";
 import { CheckOutForm } from "../components/CheckOutForm/CheckOutForm copy";
-// import { Compra, MainPage, SuccessfulPayment } from "../pages";
-import { MainPage, SuccessfulPayment } from "../pages";
+import { MainPage, SuccessfulPayment, Compra } from "../pages";
 
 
 export const router = createBrowserRouter([

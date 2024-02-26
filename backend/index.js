6b3c21edd280b090346d5987baa103cac791ad0e
marionovaro <marionovaro04@gmail.com>
2024-02-26 19:06:51 +0100

    cambios

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
-const stripe = require("stripe")("sk_test_51OkONdKjqtTHurvUcIWYNJZN68uyB6C7BYOgjzYnyqphOXFgz9bqsNAkOENKJTELiRJSkB9dQbahIc3N8vSosUTg00vsbTfdg6") //? requerimos stripe y añandimos clave secreta que nos da en el dashboard
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
+const stripe = require("stripe")(STRIPE_SECRET_KEY) //? requerimos stripe y añandimos clave secreta que nos da en el dashboard (la tenemos guardada en el .env por seguridad)
 const express = require("express")
-const app = express()
 const cors = require("cors")
+
+//! ------------- CREAMOS SERVIDOR y dar CORS-------------
+const app = express()
+app.use(cors({origin: "http://localhost:5173"}))
 
 app.use(express.static("public"))
 app.use(express.json())
-app.use(cors({origin: "http://localhost:5173"}))
 
 //! ----------- CONECTAMOS A LA BASE DE DATOS (MONGO DB) --------------
 const { conectarBd } = require("./src/utils/db")
 conectarBd()
+
+//! ----------- HACEMOS EL PAYMENT INTENT --------------
 
 const caluclateOrderAmount = (items) => { //? esto lo hacemos para que el precio esté en el backend y así no se pueda modificar
   return 15000 //? en centimos, por eso hay 2 ceros de más
@@ -28,7 +33,13 @@ app.post("/create-payment-intent", async (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+//! ----------- CREAMOS LA RUTA -----------
+
+
+
+//! ----------- PONEMOS SERVIDOR A FUNCIONAR -------------
+const PORT = process.env.PORT
+app.listen(PORT, () => {
   console.log("Servidor en el puerto", 3000)
 })
 

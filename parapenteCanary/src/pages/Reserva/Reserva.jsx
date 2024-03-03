95412b6906e3dd9b48482dfb6868c25c8dfba201
marionovaro <marionovaro04@gmail.com>
2024-03-03 18:17:42 +0100

    servicios front-back

--- parapenteCanary/src/pages/Reserva/Reserva.jsx
+++ parapenteCanary/src/pages/Reserva/Reserva.jsx
@@ -1,42 +1,95 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom"
+
 import "./Reserva.css"
 
 export const Reserva = () => {
-  const [hora, setHora] = useState()
+  const { register, handleSubmit } = useForm()
+  const navigate = useNavigate()
+
+  const [isSent, setIsSent] = useState()
+  const [res, setRes] = useState()
+
+  const formSubmit = () => {
+    setIsSent(true)
+    setRes()
+    setIsSent(false)
+  }
 
   return (
     <div id="reserva-page">
       <div id="reserva-image-container">
 
       </div>
-      <form action="submit" id="client-info-form">
+      <form action="submit" id="client-info-form" onSubmit={handleSubmit(formSubmit)}>
         <div className="input-container">
           Fecha
-          <input type="text" />
+          <input 
+            type="date"            
+            { ...register("date", {required: true})}
+          />
         </div>
         <div className="input-container radio-div">
           Hora de recogida
           <div id="button-radio-container">
-            <button onClick={() => setHora("9")}>9AM</button>
-            <button onClick={() => setHora("12")}>12AM</button>
-            <button onClick={() => setHora("15")}>3PM</button>
+            <input 
+              type="radio"
+              id="9"
+              value="9"
+              { ...register("time", {required: true})}
+            />
+            <label htmlFor="9">9AM</label>
+            <input 
+              type="radio"
+              id="12"
+              value="12"
+              { ...register("time", {required: true})}
+            />
+            <label htmlFor="12">12AM</label>
+             <input 
+              type="radio"
+              id="15"
+              value="15"
+              { ...register("time", {required: true})}
+            />
+            <label htmlFor="15">3PM</label>
           </div>
         </div>
         <div className="input-container">
+          Nombre Completo
+          <input 
+            type="text"
+            { ...register("name", {required: true})}
+          />
+        </div>
+        <div className="input-container">
           Email
-          <input type="text" />
+          <input 
+            type="email"
+            { ...register("email", {required: true})}
+          />
         </div>
         <div className="input-container">
           Número de Teléfono
-          <input type="text" />
+          <input 
+            type="text"
+            { ...register("phoneNumber", {required: true})} 
+          />
         </div>
         <div className="input-container">
           Peso (mínimo: 40kg; máximo: 100kg)
-          <input type="text" />
+          <input 
+            type="number"
+            { ...register("weight", {required: true})}
+          />
         </div>
         <div className="input-container">
           Cantidad
-          <input type="text" />
+          <input 
+            type="text"
+            { ...required("quantity", {required: true})}
+          />
         </div>
         <button>
           Ir al pago
